138f6314268ba58f5d1a4379240a0a3d20b72e9e
combondrn <combondrn@gmail.com>
2013-07-28 17:38:06 -0400

    Made stuff do stuff

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-if(typeof(Storage)!=="undefined")
-  {
-  localStorage.task2="15 minutes of guitar";
-  document.getElementById("result").innerHTML="Task: " + localStorage.task + "<br>" + localStorage.task2;
-  }
-else
-  {
-  document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
-  }
+$(document).ready(function(){
+	inIt();
+	if(typeof(Storage)!=="undefined")
+	  {
+	  updateThisOneTinyField();
+	  }
+	else
+	  {
+	  $('#result').innerHTML="Sorry, your browser does not support web storage...";
+	}
+});
+
+function inIt(){
+	if(!localStorage.display)
+		localStorage.display=JSON.stringify(['default']);
+}
+			
+function addIt() {
+	task=JSON.parse(localStorage.display);
+	task.push($('input#title.user-input').val());
+	localStorage.display=JSON.stringify(task);
+	//alert(localStorage.task);
+	updateThisOneTinyField();
+	}
+	
+function updateThisOneTinyField() {
+	//console.log(JSON.parse(localStorage.display));
+	$('#result').html("Task: " + JSON.parse(localStorage.display));
+	}
