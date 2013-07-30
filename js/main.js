a0dd6d75270f7404d57c62077f2fa63419f6b15c
combondrn <combondrn@gmail.com>
2013-07-30 18:35:07 -0400

    JS / HTML / CSS tweaks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //visual and interactions
 $(document).ready(function(){
 	// toggler
-	$('#page').on('click','a.toggler, li.js-clickable',function(){
+	$('#page').on('click','.toggler, li.js-clickable',function(){
 		var $interaction = $('div#interaction');
 		if($interaction.hasClass('visible'))
 			$interaction.animate({top:'-360px'}, 300).removeClass('visible');
