71b8fd91533b993c4f5be630d40337465dc7564d
Chris <chris@Jennifers-MacBook.local>
2013-07-30 17:40:09 -0400

    Design Layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+//visual and interactions
+$(document).ready(function(){
+	// toggler
+	$('a.toggler').click(function(){
+		var $form = $(this).parent();
+		if($form.hasClass('visible'))
+			$form.animate({top:'-360px'}, 300).removeClass('visible');
+		else {
+			$form.animate({top:'0px'}, 300).addClass('visible');
+			$form.find('input#title').focus();
+		}
+		
+	});
+});
